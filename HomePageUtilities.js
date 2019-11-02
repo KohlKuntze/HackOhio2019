39fc7b95209863ff4971a52f30e33acd42d9765a
Kyle McIntyre <kjmc0707@gmail.com>
2019-11-02 15:11:31 -0400

    session storage

--- HomePageUtilities.js
+++ HomePageUtilities.js
@@ -1,19 +1,50 @@
-<div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
+var players;
+var playerNames = [];
+
+function printPlayerNameBoxes(numOfPlayers) {
+    players = numOfPlayers;
+    var playerInputAreaDiv = document.getElementById("player-input-area");
+
+    playerInputAreaDiv.childNodes = new Array();
+    while (playerInputAreaDiv.firstChild) {
+      playerInputAreaDiv.removeChild(playerInputAreaDiv.firstChild);
+    }
     
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
-        </div>
-        <div class="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
+
+    var currentPlayer = 1;
+
+    while (currentPlayer <= numOfPlayers) {
       
-    </div>
-  </div>
+      playerInputAreaDiv.appendChild(createPlayerNameBox(currentPlayer));
+      currentPlayer = currentPlayer + 1;
+    }
+
+}
+
+function createPlayerNameBox(playerNumber) {
+  var playerBoxDiv = document.createElement("div");
+  playerBoxDiv.class = "form-group row";
+
+  var playerBoxLabel = document.createElement("label");
+  playerBoxLabel.class = "col-sm-6 col-form-label";
+  playerBoxLabel.innerHTML = "Player " + playerNumber + " Name:";  
+
+  var playerNameInput = document.createElement("input");
+  playerNameInput.id = "playerName" + playerNumber;
+  playerNameInput.type = "text";
+
+  playerBoxDiv.appendChild(playerBoxLabel);
+  playerBoxDiv.appendChild(playerNameInput);
+
+  return playerBoxDiv;
+}
+
+function startGame(){
+
+  for (i = 1; i <= players; i++){
+    var currentPlayer = "playerName"+i;
+    var playerName = document.getElementById(currentPlayer).value;
+    sessionStorage.setItem(playerName, playerName);
+  }
+  window.location.href = "game.html";
+}
